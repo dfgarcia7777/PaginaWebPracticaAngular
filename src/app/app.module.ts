c3197fbccae1581147269bb66d821de8f5c8e88c
dfgarcia7777 <dfgarcia7777@gmail.com>
2023-02-03 21:16:51 +0100

    Initial commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,30 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { RegisterFormComponent } from './components/auth/register-form/register-form.component';
+import { LoginFormComponent } from './components/auth/login-form/login-form.component';
+import { HomePageComponent } from './components/pages/home-page/home-page.component';
+import { LoginPageComponent } from './components/pages/login-page/login-page.component';
+import { RegisterPageComponent } from './components/pages/register-page/register-page.component';
+import { NotFoundPageComponent } from './components/pages/not-found-page/not-found-page.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    RegisterFormComponent,
+    LoginFormComponent,
+    HomePageComponent,
+    LoginPageComponent,
+    RegisterPageComponent,
+    NotFoundPageComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
